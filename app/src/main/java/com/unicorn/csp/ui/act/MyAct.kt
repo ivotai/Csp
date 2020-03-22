918bbab79b80410101545081d965ea3d0a3129eb
ivotai <ivotai@163.com>
2020-03-22 08:43:17 +0800

    my 2

--- app/src/main/java/com/unicorn/csp/ui/act/MyAct.kt
+++ app/src/main/java/com/unicorn/csp/ui/act/MyAct.kt
@@ -1,9 +1,47 @@
 package com.unicorn.csp.ui.act
 
+import android.graphics.drawable.GradientDrawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.blankj.utilcode.util.ConvertUtils
 import com.unicorn.csp.R
+import com.unicorn.csp.data.model.MyMenu
+import com.unicorn.csp.ui.adapter.MyAdapter
 import com.unicorn.csp.ui.base.BaseAct
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration
+import kotlinx.android.synthetic.main.act_my.*
 
 class MyAct : BaseAct() {
+
+    override fun initViews() {
+        constraintLayout1.background = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM,
+            intArrayOf(colorPrimary, blue300)
+        )
+
+        fun initRecyclerView() {
+            recyclerView.apply {
+                layoutManager = LinearLayoutManager(context)
+                simpleAdapter.bindToRecyclerView(this)
+                HorizontalDividerItemDecoration.Builder(context)
+                    .colorResId(R.color.md_grey_300)
+                    .margin(ConvertUtils.dp2px(16f), 0)
+                    .size(1)
+                    .build().let { this.addItemDecoration(it) }
+            }
+        }
+        initRecyclerView()
+    }
+
+    override fun bindIntent() {
+        simpleAdapter.setNewData(MyMenu.all)
+    }
+
+    private val simpleAdapter = MyAdapter()
+
+    private val colorPrimary by lazy { ContextCompat.getColor(this, R.color.colorPrimary) }
+    private val blue300 by lazy { ContextCompat.getColor(this, R.color.md_red_300) }
 
     override val layoutId = R.layout.act_my
 
---
+++ app/src/main/java/com/unicorn/csp/ui/adapter/MyAdapter.kt
@@ -0,0 +1,26 @@
+package com.unicorn.csp.ui.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.unicorn.csp.R
+import com.unicorn.csp.app.safeClicks
+import com.unicorn.csp.data.model.MyMenu
+import com.unicorn.csp.ui.base.KVHolder
+import kotlinx.android.synthetic.main.item_my.*
+
+class MyAdapter : BaseQuickAdapter<MyMenu, KVHolder>(R.layout.item_my) {
+
+    override fun convert(helper: KVHolder, item: MyMenu) {
+        helper.apply {
+            tvText.text = item.text
+            root.safeClicks().subscribe {
+                when (item) {
+
+//
+                    else -> {
+                    }
+                }
+            }
+        }
+    }
+
+}
