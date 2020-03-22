5a20dab696b9aa87409c70201370fc45972c52b1
ivotai <ivotai@163.com>
2020-03-22 11:36:47 +0800

    按小顾意见修改。

--- app/src/main/java/com/unicorn/csp/data/model/MyMenu.kt
+++ app/src/main/java/com/unicorn/csp/data/model/MyMenu.kt
@@ -1,13 +1,16 @@
 package com.unicorn.csp.data.model
 
+import com.blankj.utilcode.util.AppUtils
+
 enum class MyMenu(val text: String) {
 
     ModifyPassword("修改密码"),
-    Logout("退出登录")
+    Logout("退出登录"),
+    VersionName("版本号：${AppUtils.getAppVersionName()}")
     ;
 
     companion object {
-        val all get() = listOf(ModifyPassword, Logout)
+        val all get() = listOf(ModifyPassword, Logout, VersionName)
     }
 
 }
