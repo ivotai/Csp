93fcf2ef95644cf20fc2613ad341e0a287b5ebf1
ivotai <ivolianer2015@163.com>
2020-06-13 13:37:51 +0800

    添加 搜索框。

--- app/src/main/java/com/unicorn/csp/ui/act/ArticleSearchAct.kt
+++ app/src/main/java/com/unicorn/csp/ui/act/ArticleSearchAct.kt
@@ -1,10 +1,22 @@
 package com.unicorn.csp.ui.act
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import com.blankj.utilcode.util.ConvertUtils
 import com.unicorn.csp.R
 import com.unicorn.csp.ui.base.BaseAct
+import kotlinx.android.synthetic.main.act_article_search.*
 
 class ArticleSearchAct : BaseAct() {
 
     override val layoutId = R.layout.act_article_search
+
+    override fun initViews() {
+        super.initViews()
+        etSearch.background = GradientDrawable().apply {
+            setColor(Color.WHITE)
+            cornerRadius = ConvertUtils.dp2px(10f).toFloat()
+        }
+    }
 
 }
